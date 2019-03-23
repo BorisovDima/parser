f865d8be20e4da5d43b323425518051a2fefefd6
Your Name <you@example.com>
2019-03-24 02:00:55 +0300

    -

--- configs.py
+++ configs.py
@@ -4,11 +4,11 @@ from functools import partial
 base = partial(os.path.join, os.path.dirname(__file__))
 
 class Config:
-    DB_NAME = base(os.environ.get('DB_NAME') or 'database')
+    DB_NAME = base((os.environ.get('DB_NAME') or 'database') + '.db')
     DEBUG = os.environ.get('DEBUG') or False
 
 
 
 class TestConfig:
-    DB_NAME = base(os.environ.get('DB_NAME') or 'database.test')
+    DB_NAME = base('database.test')
 
